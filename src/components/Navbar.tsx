44f0cb72a6e4882f99f78cf980c0486d3abe36f1
Nelson Foltz <nelsfoltz@gmail.com>
2024-04-20 15:22:45 -0400

    WIP: dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
 import Hamburger from './icons/Hamburger';
 import { SignedIn, UserButton, SignOutButton } from '@clerk/nextjs';
 import classNames from 'classnames';
-import { CaretDownIcon } from '@radix-ui/react-icons';
+import { CaretDownIcon, HamburgerMenuIcon, DotFilledIcon, CheckIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import './styles.css';
 
 // Define a type for navigation items
@@ -175,6 +176,101 @@ const ListItem =
 // };
 
 
+const NavDropdownMenu = () => {
+  const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
+  const [urlsChecked, setUrlsChecked] = React.useState(false);
+  const [person, setPerson] = React.useState('pedro');
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className="NavigationMenuLink" aria-label="view maps">
+          Maps
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Tab <div className="RightSlot">⌘+T</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Window <div className="RightSlot">⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem" disabled>
+            New Private Window <div className="RightSlot">⇧+⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
+              More Tools
+              <div className="RightSlot">
+                <ChevronRightIcon />
+              </div>
+            </DropdownMenu.SubTrigger>
+            <DropdownMenu.Portal>
+              <DropdownMenu.SubContent
+                className="DropdownMenuSubContent"
+                sideOffset={2}
+                alignOffset={-5}
+              >
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Save Page As… <div className="RightSlot">⌘+S</div>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">Create Shortcut…</DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">Name Window…</DropdownMenu.Item>
+                <DropdownMenu.Separator className="DropdownMenu.Separator" />
+                <DropdownMenu.Item className="DropdownMenuItem">Developer Tools</DropdownMenu.Item>
+              </DropdownMenu.SubContent>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Sub>
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.CheckboxItem
+            className="DropdownMenuCheckboxItem"
+            checked={bookmarksChecked}
+            onCheckedChange={setBookmarksChecked}
+          >
+            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+              <CheckIcon />
+            </DropdownMenu.ItemIndicator>
+            Show Bookmarks <div className="RightSlot">⌘+B</div>
+          </DropdownMenu.CheckboxItem>
+          <DropdownMenu.CheckboxItem
+            className="DropdownMenuCheckboxItem"
+            checked={urlsChecked}
+            onCheckedChange={setUrlsChecked}
+          >
+            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+              <CheckIcon />
+            </DropdownMenu.ItemIndicator>
+            Show Full URLs
+          </DropdownMenu.CheckboxItem>
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.Label className="DropdownMenuLabel">People</DropdownMenu.Label>
+          <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
+            <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="pedro">
+              <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                <DotFilledIcon />
+              </DropdownMenu.ItemIndicator>
+              Pedro Duarte
+            </DropdownMenu.RadioItem>
+            <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="colm">
+              <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                <DotFilledIcon />
+              </DropdownMenu.ItemIndicator>
+              Colm Tuite
+            </DropdownMenu.RadioItem>
+          </DropdownMenu.RadioGroup>
+
+          <DropdownMenu.Arrow className="DropdownMenuArrow" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -214,6 +310,10 @@ const Navbar = () => {
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
+        <NavigationMenu.Item>
+          <NavDropdownMenu />
+        </NavigationMenu.Item>
+
          
         <SignedIn>
           <NavigationMenu.Item>
@@ -225,11 +325,13 @@ const Navbar = () => {
           </NavigationMenu.Item>
         </SignedIn>
 
+        {/* Delete if not being used */}
         <NavigationMenu.Indicator className="NavigationMenuIndicator">
           <div className="Arrow" />
         </NavigationMenu.Indicator>
       </NavigationMenu.List>
 
+        {/* Delete if not being used */}
       <div className="ViewportPosition">
         <NavigationMenu.Viewport className="NavigationMenuViewport" />
       </div>
