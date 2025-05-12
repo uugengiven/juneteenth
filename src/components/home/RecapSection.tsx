ca0c579c4d492818587bca46a0d6f8db6fbafc68
John Lange <lange.john.m@gmail.com>
2025-05-12 01:24:44 -0400

    Update video

--- src/components/home/RecapSection.tsx
+++ src/components/home/RecapSection.tsx
@@ -5,7 +5,7 @@ const RecapSection:React.FC = () => {
             <h3 className="text-6xl font-bold text-grey-900 m-4">Juneteenth 2023 Recap</h3>
             <div className="aspect-video mx-4 rounded-lg overflow-hidden">
                 <iframe
-                src="https://www.youtube.com/embed/SjnFdRibjks?si=RuplTlZBaC9RVlKe"
+                src="https://www.youtube.com/embed/5iTGaC51kwQ?si=RuplTlZBaC9RVlKe"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
