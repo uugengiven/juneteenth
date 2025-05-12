c43b4f9535ada95ddb66e6f39baff6d5a2ff41f2
John Lange <lange.john.m@gmail.com>
2025-05-12 01:26:46 -0400

    Update language on home page

--- src/components/home/RecapSection.tsx
+++ src/components/home/RecapSection.tsx
@@ -2,7 +2,7 @@ const RecapSection:React.FC = () => {
     return(
         <section className="bg-white pt-4 pb-12">
             <div className="mt-12 text-center">
-            <h3 className="text-6xl font-bold text-grey-900 m-4">Juneteenth 2023 Recap</h3>
+            <h3 className="text-6xl font-bold text-grey-900 m-4">Juneteenth 2025 Is Here!</h3>
             <div className="aspect-video mx-4 rounded-lg overflow-hidden">
                 <iframe
                 src="https://www.youtube.com/embed/5iTGaC51kwQ?si=RuplTlZBaC9RVlKe"
